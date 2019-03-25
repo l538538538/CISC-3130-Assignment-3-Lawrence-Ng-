f74cf5c0d7e45152a56b44fc830a58c79c958153
Lawrence Ng <Lawrence.ng2013@gmail.com>
2019-03-25 09:45:07 -0400

    Assignment 3 part 2

---
+++ FrequencyList.java
@@ -0,0 +1,44 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+public class FrequencyList {
+        public static void main(String[]args) throws IOException{
+              
+        FileInputStream read = new  FileInputStream("song.txt");
+        Scanner fileInput = new Scanner(read);
+
+        ArrayList<String> words = new ArrayList<String>();
+        ArrayList<Integer> count = new ArrayList<Integer>();
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+
+        while(fileInput.hasNext()){
+        	String nextWord = fileInput.next();
+        	if(words.contains(nextWord)){
+        		int index = words.indexOf(nextWord);
+        		count.set(index, count.get(index) + 1);
+        		}
+        		else{
+        			words.add(nextWord);
+        			count.add(1);
+        			}
+             }
+        
+        for(int i = 0; i < words.size(); i++){
+        	result.put(words.get(i), count.get(i));
+        }
+
+        fileInput.close();
+        read.close();
+        
+        Set set = result.entrySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()) {
+        	Map.Entry mentry = (Map.Entry)iterator.next();
+        	System.out.print(mentry.getValue() + " : " );
+        	System.out.println(mentry.getKey());
+        	
+      }
+
+}
+}
